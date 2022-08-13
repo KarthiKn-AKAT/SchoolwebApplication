07987db5581ea4c7259c351baf862ac0bd6d7f9b
Karthi678 <1801023c@cit.edu.in>
2022-08-13 19:17:55 +0530

    Initial commit

---
+++ src/main/java/com/karthi/SchoolApplication/SchoolApplication.java
@@ -0,0 +1,17 @@
+package com.karthi.SchoolApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SchoolApplication.class, args);
+
+
+
+
+	}
+
+}
